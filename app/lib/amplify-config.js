2c84fd94aab7f9b559bf8389e91e0687b1dd8cd3
dheeraj <dheeraj>
2025-03-30 08:49:44 +0530

    ...

--- app/lib/amplify-config.js
+++ app/lib/amplify-config.js
@@ -1,19 +1,14 @@
-import { Amplify } from 'aws-amplify';
+import { Amplify, API } from 'aws-amplify';
 
-// Initialize AWS Amplify
+
 Amplify.configure({
-  // AWS Region - will be set from environment variables
-  aws_project_region: process.env.NEXT_PUBLIC_AWS_REGION,
-  
-  // AppSync (GraphQL) configuration
-  aws_appsync_graphqlEndpoint: process.env.NEXT_PUBLIC_APPSYNC_ENDPOINT,
-  aws_appsync_region: process.env.NEXT_PUBLIC_AWS_REGION,
-  aws_appsync_authenticationType: 'API_KEY',
-  aws_appsync_apiKey: process.env.NEXT_PUBLIC_APPSYNC_API_KEY,
-  
-  // Analytics is disabled by default
-  Analytics: {
-    disabled: true
+  API: {
+    GraphQL: {
+      endpoint: process.env.NEXT_PUBLIC_APPSYNC_ENDPOINT,
+      region: process.env.NEXT_PUBLIC_AWS_REGION,
+      defaultAuthMode: 'apiKey',
+      apiKey: process.env.NEXT_PUBLIC_APPSYNC_API_KEY
+    }
   }
 });
 
