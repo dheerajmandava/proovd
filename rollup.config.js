785ba91994ff8c4a563f6ff1d7c4307b22be3471
dheeraj <dheeraj>
2025-03-31 21:50:40 +0530

    ...

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,11 @@
-const { nodeResolve } = require('@rollup/plugin-node-resolve');
-const commonjs = require('@rollup/plugin-commonjs');
-const typescript = require('@rollup/plugin-typescript');
-const terser = require('@rollup/plugin-terser');
-const replace = require('@rollup/plugin-replace');
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
 
-module.exports = {
+export default {
   input: 'src/pulse-widget/index.ts',
   output: {
     file: 'public/pulse-widget.min.js',
@@ -15,27 +14,13 @@ module.exports = {
     sourcemap: !production
   },
   plugins: [
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(production ? 'production' : 'development'),
-      preventAssignment: true
-    }),
     nodeResolve({
-      browser: true,
+      browser: true
     }),
     commonjs(),
     typescript({
-      tsconfig: false,
-      compilerOptions: {
-        target: "es2020",
-        module: "ESNext",
-        moduleResolution: "node",
-        esModuleInterop: true,
-        sourceMap: !production,
-        inlineSources: !production,
-        lib: ["dom", "es2020"],
-        strict: true
-      },
-      include: ["src/pulse-widget/**/*.ts"]
+      tsconfig: './src/pulse-widget/tsconfig.json',
+      sourceMap: !production
     }),
     production && terser()
   ]
